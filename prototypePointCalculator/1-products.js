8f3a5e31a79fd44ce0c1cbc8565b6a06563ddf1b
Bhall96 <105361442+Bhall96@users.noreply.github.com>
2022-11-01 21:25:58 -0400

    work in progress buttons just for reference

---
+++ prototypePointCalculator/1-products.js
@@ -0,0 +1,42 @@
+var products = {
+  1: {
+    name : "Small Cardboard Box",
+    desc : "smaller boxes small aplliance / shoe box or smaller",
+    price : 2
+  },
+  2: {
+    name : "Large Cardboard Box",
+    desc : "boxes Larger then you typical small appliance box",
+    price : 4
+  },
+  3: {
+    name : "Plastic Store Bag",
+    desc : "grocerie stor bags",
+    price : 1
+  },
+  4: {
+    name : "Can",
+    desc : "aluminum cans",
+    price : 1
+  },
+  5 : {
+    name : "Plastic Bottle",
+    desc : "plastic water or soda bottles",
+    price : 1
+  },
+  6 : {
+    name : "Large Glass Bottle",
+    desc : "Larger glass containers like wine bottles",
+    price : 3
+  },
+  7 : {
+    name : "Small Glass Bottle",
+    desc : "small glass containers such as pop or beer bottles",
+    price : 1
+  },
+  8 : {
+    name : "Alternative Transport per Mile",
+    desc : "walking, biking , or carpooling",
+    price : 5
+  }
+};
