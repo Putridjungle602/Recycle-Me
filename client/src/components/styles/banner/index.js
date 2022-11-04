b29aa86d2d89eef3d75a1e15991b45517c0da512
Milante <milante.adams@yahoo.com>
2022-11-04 19:27:02 -0400

    added styles for components

---
+++ client/src/components/styles/banner/index.js
@@ -0,0 +1,59 @@
+import { Box } from "@material-ui/core";
+import { Colors } from "../theme";
+import { styled } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+export const BannerContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  width: "100%",
+  height: "100%",
+  padding: "0px 0px",
+  background: Colors.light_brown,
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
+
+export const BannerImage = styled("img")(({ src, theme }) => ({
+  src: `url(${src})`,
+  width: "500px",
+  [theme.breakpoints.down("md")]: {
+    width: "350px",
+  },
+
+  [theme.breakpoints.down("sm")]: {
+    width: "320px",
+    height: "300px",
+  },
+}));
+
+export const BannerContent = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  maxWidth: 420,
+  padding: "30px",
+}));
+
+export const BannerTitle = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.5,
+  fontSize: "72px",
+  marginBottom: "20px",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "42px",
+  },
+}));
+
+export const BannerDescription = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.25,
+  letterSpacing: 1.25,
+  marginBottom: "3em",
+  [theme.breakpoints.down("md")]: {
+    lineHeight: 1.15,
+    letterSpacing: 1.15,
+    marginBottom: "1.5em",
+  },
+}));
---
+++ client/src/components/styles/footer/index.js
@@ -0,0 +1,6 @@
+import { styled, Typography } from "@material-ui/core";
+
+export const FooterTitle = styled(Typography)(() => ({
+  textTransform: "uppercase",
+  marginBottom: "1em",
+}));
